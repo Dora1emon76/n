ee0a92a94cca2150ca76e0086d20b1e2b7d1cbb7
Yusuf Vohra <121709213+Dora1emon76@users.noreply.github.com>
2023-09-22 18:22:17 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
 import pytesseract
 from PIL import Image
-import
+import requests
+from io import BytesIO
+
+# URL of the image
+image_url = 'https://graph.org/file/3e1776a2ece811418f1fe.jpg'
+
+# Download the image from the URL
+response = requests.get(image_url)
+image = Image.open(BytesIO(response.content))
+
+# Use pytesseract to do OCR on the image
+text = pytesseract.image_to_string(image)
+
+# Print the extracted text
+print(text)
